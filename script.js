5a7b8cf898cd2ac8dbc909a3b2dbeb0f4434203e
ryldonsalvador <50676729+ryldonsalvador@users.noreply.github.com>
2022-06-30 08:54:05 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function weatherBalloon( cityID ) {
     return resp.json() 
   }) // Convert data to json
   .then(function(data) {
-    console.log(data)
+    // console.log(data)
     drawWeather(data); // Call drawWeather
   })
   .catch(function() {
@@ -29,7 +29,6 @@ function drawWeather( d ) {
   // var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
   var description = d.weather[0].description;
 
-  // console.log(celcius)
   
   document.getElementById('description').innerHTML = description;
   document.getElementById('temp').innerHTML = celcius + '&deg;';
@@ -49,64 +48,20 @@ function drawWeather( d ) {
 
 
 window.onload = function() {
-  // weatherBalloon( 1730737 );
-  
-  setTimeout(function(){
-    callInit();  
-  },300);
+  weatherBalloon( 1730737 );
+  // callInit();  
 }
 
-
-// 5128581 - New York 
-// 6167865 - Toronto
-// 5368361 - Los Angeles
-
 function callInit(){
-  // var getIP = 'http://ip-api.com/json/';
-  // $.getJSON(getIP).done(function(location) {
-  //     console.log(location)
-  // })
 
-  // var getIP = 'http://ip-api.com/json/';
-  // var openWeatherMap = 'https://api.openweathermap.org/data/2.5/weather'
-  // $.getJSON(getIP).done(function(location) {
-  //     $.getJSON(openWeatherMap, {
-  //         lat: location.lat,
-  //         lon: location.lon,
-  //         units: 'metric',
-  //         appid: 'aa47ed22f59d85790bac334cd475e6e5'
-  //     }).done(function(weather) {
-  //         console.log(weather)
-  //         weatherOutput(weather);
-  //     })
-  // })
-
-
-  // var openWeatherMap = 'https://api.openweathermap.org/data/2.5/weather'
-  // if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(function(position) {
-  //         $.getJSON(openWeatherMap, {
-  //             lat: position.coords.latitude,
-  //             lon: position.coords.longitude,
-  //             units: 'metric',
-  //             appid: 'aa47ed22f59d85790bac334cd475e6e5'
-  //         }).done(function(weather) {
-  //             console.log(weather)
-  //             weatherOutput(weather);
-  //         })
-  //     })
-  // }
-  var lat;
-  var lon;
 
   $.getJSON("https://ip-api.io/json/",
     function(result) {
-        console.log(result);
-        console.log(result.latitude);
-        console.log(result.longitude);
+        // console.log(result);
+        // console.log(result.latitude);
+        // console.log(result.longitude);
         lat = result.latitude;
         lon = result.longitude;
-
 
         var key = 'aa47ed22f59d85790bac334cd475e6e5';
         fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=metric&appid=' + key)
@@ -120,6 +75,7 @@ function callInit(){
           // catch any errors
         });
     });
+
 }
 
 function weatherOutput (d){
